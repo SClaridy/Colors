56ca92bd6b26d431221bf551e47c5b3ee4559bfa
SClaridy <91565748+SClaridy@users.noreply.github.com>
2021-10-04 15:13:07 -0500

    Add files via upload

---
+++ circle function.py
@@ -0,0 +1,14 @@
+#Initialization
+import turtle
+import random
+shea=turtle.Turtle()
+shea.left(90)
+import random
+#Function
+def randomDot():
+    shea.color("blue")
+    shea.begin_fill()
+    shea.end_fill()
+    shea.circle(100)
+#Main
+randomDot()
